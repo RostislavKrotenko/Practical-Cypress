97beadeebda94640726bf3ee09c9d5e2c855d433
RostislavKrotenko <rostik.krotenko@gmail.com>
2023-11-21 16:44:13 +0200

    add new test to layoutTest and update layoutPage

--- cypress/e2e/layoutTest.js
+++ cypress/e2e/layoutTest.js
@@ -14,4 +14,11 @@ describe('Test Layout Page', () => {
         formLayoutPage.submitInlineFormWithNameAndEmail('Kelin', 'test@test.com')
         formLayoutPage.submitBasicFormWithNameAndEmail('rost.krot@gmail.com', '12345')
     })
+
+    it('Test Using the Grid, Form without labels, Block, Horizontal Forms', () => {
+        formLayoutPage.submitUsingTheGridFormWithEmailAndPassword('r_krot@gmail.com', 'IloveMole', 'Option 2')
+        formLayoutPage.submitFormWithoutLabels('J. Bodra', 'Access related issue', 'Don`t have access to necessary applications')
+        formLayoutPage.submitBlockForm('Rostyslav', 'Krotenko', 'rost@gmail.com', 'https://softserve.udemy.com/course/cypress-web-automation-testing-from-zero-to-hero')
+        formLayoutPage.submitHorizontalForm(' var_cina@yahoo.com', '34Yt*&!jio', true)
+    })
 })
--- cypress/support/page_object/layoutPage.js
+++ cypress/support/page_object/layoutPage.js
@@ -20,6 +20,45 @@ export class FormLayoutPage {
         })
     }
 
+    submitUsingTheGridFormWithEmailAndPassword(userEmail, userPassword, radioButton='Option 1') {
+        cy.contains('nb-card', 'Using the Grid').find('form').then( form => {
+            cy.wrap(form).find('[placeholder="Email"]').type(userEmail)
+            cy.wrap(form).find('[placeholder="Password"]').type(userPassword)
+            cy.wrap(form).contains('nb-radio', radioButton).find('input').click({force: true})
+            cy.wrap(form).submit()
+        })
+    }
+
+    submitFormWithoutLabels(recipients, subject, message) {
+        cy.contains('nb-card', 'Form without labels').find('form').then( form => {
+            cy.wrap(form).find('[placeholder="Recipients"]').type(recipients)
+            cy.wrap(form).find('[placeholder="Subject"]').type(subject)
+            cy.wrap(form).find('[placeholder="Message"]').type(message)
+            cy.wrap(form).submit()
+        })
+    }
+
+    submitBlockForm(firstName, lastName, email, website) {
+        cy.contains('nb-card', 'Block form').then( form => {
+            cy.wrap(form).find('[placeholder="First Name"]').type(firstName)
+            cy.wrap(form).find('[placeholder="Last Name"]').type(lastName)
+            cy.wrap(form).find('[placeholder="Email"]').type(email)
+            cy.wrap(form).find('[placeholder="Website"]').type(website)
+            cy.wrap(form).find('button').click()
+        })
+    }
+
+    submitHorizontalForm(email, password, rememberMe=false) {
+        cy.contains('nb-card', 'Horizontal form').find('form').then( form => { 
+            cy.wrap(form).find('[placeholder="Email"]').type(email)
+            cy.wrap(form).find('[placeholder="Password"]').type(password)
+            if (rememberMe) {
+                cy.wrap(form).find('[type="checkbox"]').check({force: true})
+            }
+            cy.wrap(form).submit()
+        })
+    }
+
 }
 
 export const formLayoutPage = new FormLayoutPage()
